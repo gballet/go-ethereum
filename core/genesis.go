e4db3f9841c009f6188cf2015c176735b7638071
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-02-08 18:38:38 +0530

    Remove

--- core/genesis.go
+++ core/genesis.go
@@ -470,8 +470,6 @@ func (g *Genesis) configOrDefault(ghash common.Hash) *params.ChainConfig {
 // ToBlock returns the genesis block according to genesis specification.
 func (g *Genesis) ToBlock() *types.Block {
 	root, err := g.Alloc.deriveHash(g.Config)
-	fmt.Print("State Root: ")
-	fmt.Println(root.Hex())
 	if err != nil {
 		panic(err)
 	}
