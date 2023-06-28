7be87aa6012ec4bfe4706482f0abc8f7a02af518
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-06-28 15:05:16 +0200

    move state import to its own function in nethermind command

--- cmd/geth/nethermind.go
+++ cmd/geth/nethermind.go
@@ -91,104 +91,7 @@ processing will proceed even if an individual RLP-file import failure occurs.`,
 	}
 )
 
-func nethermindImport(ctx *cli.Context) error {
-	if ctx.Args().Len() < 2 {
-		utils.Fatalf("This command requires two arguments.")
-	}
-	// // Start metrics export if enabled
-	// utils.SetupMetrics(ctx)
-	// // Start system runtime metrics collection
-	// go metrics.CollectProcessMetrics(3 * time.Second)
-
-	stack, _ := makeConfigNode(ctx)
-	defer stack.Close()
-
-	// chain, db := utils.MakeChain(ctx, stack, false)
-	db := utils.MakeChainDatabase(ctx, stack, false)
-	triedb := trie.NewDatabase(db)
-	core.SetupGenesisBlock(db, triedb, core.DefaultGnosisGenesisBlock())
-	// defer db.Close()
-
-	// Start periodically gathering memory profiles
-	// var peakMemAlloc, peakMemSys uint64
-	// go func() {
-	// 	stats := new(runtime.MemStats)
-	// 	for {
-	// 		runtime.ReadMemStats(stats)
-	// 		if atomic.LoadUint64(&peakMemAlloc) < stats.Alloc {
-	// 			atomic.StoreUint64(&peakMemAlloc, stats.Alloc)
-	// 		}
-	// 		if atomic.LoadUint64(&peakMemSys) < stats.Sys {
-	// 			atomic.StoreUint64(&peakMemSys, stats.Sys)
-	// 		}
-	// 		time.Sleep(5 * time.Second)
-	// 	}
-	// }()
-	// Import the chain
-	start := time.Now()
-
-	// Connect to the Ethereum node over RPC// Connect to the Ethereum node over RPC
-	// client, err := ethclient.Dial("https://localhost:8545")
-	// if err != nil {
-	// 	log.Crit("Failed to connect to the Ethereum node: %v", err)
-	// }
-
-	// Get the latest block number
-	// latestBlock, err := client.BlockNumber(context.Background())
-	// if err != nil {
-	// 	log.Crit("Failed to get latest block number: %v", err)
-	// }
-
-	// Create a LevelDB database for storing the RLP of the blocks
-	// dbPath := filepath.Join(".", "blocksdb")
-	// os.MkdirAll(dbPath, os.ModePerm)
-	// db, err := rawdb.NewLevelDBDatabase(dbPath, 0, 0, "")
-	// if err != nil {
-	// 	log.Fatalf("Failed to create LevelDB database: %v", err)
-	// }
-
-	// Iterate over all the blocks since the genesis and insert their RLP in the database
-	// var headRoot common.Hash
-	// for i := uint64(0); i <= latestBlock; i++ {
-	// 	block, err := client.BlockByNumber(context.Background(), new(big.Int).SetUint64(i))
-	// 	if err != nil {
-	// 		log.Crit("Failed to get block", "number", i, "error", err)
-	// 	}
-
-	// 	// Insert the RLP of the block in the LevelDB database
-	// 	rawdb.WriteBlock(db, block)
-	var firstblock types.Block
-	firstblockrlp := common.Hex2Bytes("f90246f90241a04f1dd23188aab3a76b463e4af801b52b1248ef073c648cbdc4c9333d3da79756a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d4934794cace5b3c29211740e595850e80478416ee77ca21a040cf4430ecaa733787d1a65154a3b9efb560c95d9e324a23b97f0609b539133ba056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000090ffffffffffffffffffffffffeda7455a018398968080845bbba5399fde830201018f5061726974792d457468657265756d86312e32392e30826c69841258baa5b841c35e9f8ac05c69d8c36af2cb8b7bb3beda5945e8367fec758c459d32f9ddc183206bad888ebd7addaa6e3f00593100a628c5e86ca80ebe48d4fc831dd36f825d01c0c0")
-	fmt.Println(firstblockrlp)
-	err := rlp.DecodeBytes(firstblockrlp, &firstblock)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(firstblock.NumberU64())
-	rawdb.WriteBlock(db, &firstblock)
-
-	blockrlp := common.Hex2Bytes("f90209f90204a0985fbf7e42f24f40cb5c8e74dc8df128f1ffb8bc3d5ee85392cdd78227cfe7a9a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347940c36ac8863d0352436a70addbf1be34f3ea11017a0833490ba0cda8dc41d84e51fbd9a2debd0bbd6bf2647387b51be578236794e69a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000080840197ebdb8401c9c380808464000d938a4e65746865726d696e64a004630c37f571827ddb23b79e4d5cb7e29c92288b4d53ea1b1fd077c3d33774c988000000000000000007c0c0")
-	var block types.Block
-	fmt.Println(blockrlp)
-	err = rlp.DecodeBytes(blockrlp, &block)
-	if err != nil {
-		panic(err)
-	}
-	// 	if i == latestBlock {
-	// 		copy(headRoot[:], block.Root().Bytes())
-	rawdb.WriteHeadHeaderHash(db, block.Hash())
-	rawdb.WriteHeadBlockHash(db, block.Hash())
-	rawdb.WriteBlock(db, &block)
-	rawdb.WriteHeaderNumber(db, block.Hash(), block.NumberU64())
-	rawdb.WriteCanonicalHash(db, block.Hash(), block.NumberU64())
-	rawdb.WriteFinalizedBlockHash(db, block.Hash())
-	rawdb.WriteHeadBlockHash(db, block.Hash())
-	rawdb.WriteHeadFastBlockHash(db, block.Hash())
-	fmt.Println("wrote head block", block.Hash())
-	// 	}
-	// }
-
-	///// LECTURE DU FICHIER /////////////////////////////////////////
+func importStateFromFile(db ethdb.Database) {
 	// Import snapshot from file
 	batch := db.NewBatch()
 	inputFile, err := os.Open("state.txt")
@@ -208,8 +111,8 @@ func nethermindImport(ctx *cli.Context) error {
 		savenode        = func(owner common.Hash, path []byte, hash common.Hash, blob []byte) {
 			rawdb.WriteTrieNode(batch, hash, blob)
 		}
-		accTrie = trie.NewStackTrie(savenode)
-		// headRoot = common.HexToHash("833490ba0cda8dc41d84e51fbd9a2debd0bbd6bf2647387b51be578236794e69")
+		accTrie  = trie.NewStackTrie(savenode)
+		headRoot = common.HexToHash("833490ba0cda8dc41d84e51fbd9a2debd0bbd6bf2647387b51be578236794e69")
 	)
 
 	// Read each line and process the hex values
@@ -283,27 +186,55 @@ func nethermindImport(ctx *cli.Context) error {
 			batch = db.NewBatch()
 		}
 	}
+	if lastStorageRoot != (common.Hash{}) {
+		log.Info("processing storage for last account")
+		_, err := stTrie.Commit()
+		if err != nil {
+			panic(err)
+		}
+		if stTrie.Hash() != lastStorageRoot {
+			fmt.Printf("invalid storage root %x != %x\n", stTrie.Hash(), lastStorageRoot)
+			panic("ici")
+		}
+		stTrie = nil
+	}
 	h, err := accTrie.Commit()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(h)
-	// if accTrie.Hash() != headRoot {
-	// 	panic("incorrect root hash")
-	// }
+	if accTrie.Hash() != headRoot {
+		panic("incorrect root hash")
+	}
 	if err := batch.Write(); err != nil {
 		panic(err)
 	}
 	/////////////////////////////////////////////////////////////
 
 	// ////////////////// REBUILD SNAPSHOT //////////////////////
-	_, err = snapshot.New(snapshot.Config{CacheSize: 2048}, db, trie.NewDatabase(db) /* headRoot */, accTrie.Hash())
+	_, err = snapshot.New(snapshot.Config{CacheSize: 2048}, db, trie.NewDatabase(db), headRoot)
 	if err != nil {
 		panic(err)
 	}
 	// if err := snaptree.Cap( /* headRoot */ accTrie.Hash(), 0); err != nil {
 	// 	panic(err)
 	// }
+}
+
+func nethermindImport(ctx *cli.Context) error {
+	if ctx.Args().Len() < 2 {
+		utils.Fatalf("This command requires two arguments.")
+	}
+
+	stack, _ := makeConfigNode(ctx)
+	defer stack.Close()
+
+	// chain, db := utils.MakeChain(ctx, stack, false)
+	db := utils.MakeChainDatabase(ctx, stack, false)
+	triedb := trie.NewDatabase(db)
+	core.SetupGenesisBlock(db, triedb, core.DefaultGnosisGenesisBlock())
+
+	// importStateFromFile(db)
 	db.Close()
 	/////////////////////////////////////////////////////////////
 
