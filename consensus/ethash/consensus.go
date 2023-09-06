dc910f1c980310f071483d58e795bcb00f8a9f57
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-09-06 10:51:07 +0200

    use prague as the verkle activation fork

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -567,7 +567,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 		r.Div(r, big8)
 
 		// This should not happen, but it's useful for replay tests
-		if config.IsVerkle(header.Number, header.Time) {
+		if config.IsPrague(header.Number, header.Time) {
 			uncleCoinbase := utils.GetTreeKeyBalance(uncle.Coinbase.Bytes())
 			state.Witness().TouchAddressOnReadAndComputeGas(uncleCoinbase)
 		}
@@ -576,7 +576,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 		r.Div(blockReward, big32)
 		reward.Add(reward, r)
 	}
-	if config.IsVerkle(header.Number, header.Time) {
+	if config.IsPrague(header.Number, header.Time) {
 		coinbase := utils.GetTreeKeyBalance(header.Coinbase.Bytes())
 		state.Witness().TouchAddressOnReadAndComputeGas(coinbase)
 		coinbase[31] = utils.VersionLeafKey // mark version
