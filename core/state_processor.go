e0cf3fcc57794fe7ecb1cef7f7d9b1e9e5748d3c
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-02-14 16:43:15 +0100

    fix stupid bug

--- core/state_processor.go
+++ core/state_processor.go
@@ -61,7 +61,7 @@ func NewStateProcessor(config *params.ChainConfig, bc *BlockChain, engine consen
 // returns the amount of gas that was used in the process. If any of the
 // transactions failed to execute due to insufficient gas it will return an error.
 func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg vm.Config) (types.Receipts, []*types.Log, uint64, error) {
-	fmt.Println("process block", statedb.Database().GetCurrentAccountAddress(), statedb.Database().GetCurrentAccountHash())
+	fmt.Println("process block", block.NumberU64())
 	var (
 		receipts    types.Receipts
 		usedGas     = new(uint64)
