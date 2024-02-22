009d3e2abece7ce208e1080d9665bd0330443a1c
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-02-22 16:03:51 +0100

    persist conversion state to db and use an LRU cache for active transition states (#375)
    * persist conversion state to db
    
    * fix: don't recreate LRU when writing state
    
    * opt: only write state to db if not already present in LRU
    
    * fix: rlp can't encode TransitionState
    
    * fix: use gob because binary.Write does not support structs 🤦‍♂️
    
    * fix: nil pointer panic
    
    * add logs to debug shadowfork
    
    * no such thing as not enough traces
    
    * ditto
    
    * fix stupid bug
    
    * add a comment for readability
    
    * add more traces
    
    * Lock the state transition during conversion (#384)
    
    * heavy handed approach: lock the state transition during conversion
    
    * also lock transition state loading/unloading
    
    * reduce logs verbosity
    
    * add conversion test to workflow (#386)
    
    * add conversion test to workflow
    
    * mandatory -f switch fix in rm
    
    * forgot & at the end of the geth command
    
    * remove incorrect kill
    
    * add debug traces
    
    * add an overlay stride
    
    * fix typo
    
    * Apply suggestions from code review

--- core/state_processor.go
+++ core/state_processor.go
@@ -107,12 +107,6 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		return nil, nil, 0, errors.New("withdrawals before shanghai")
 	}
 
-	// Perform the overlay transition, if relevant
-	//parent := p.bc.GetHeaderByHash(header.ParentHash)
-	//if err := OverlayVerkleTransition(statedb, parent.Root); err != nil {
-	//	return nil, nil, 0, fmt.Errorf("error performing verkle overlay transition: %w", err)
-	//}
-
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions(), block.Uncles(), withdrawals)
 
