0b9d42bfaeec40605eac57886bb56e1ad802b4ca
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-09-06 14:56:46 +0200

    remove redundant call

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -525,11 +525,6 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 				err = ErrCodeStoreOutOfGas
 			}
 		} else {
-			// Contract creation completed, touch the missing fields in the contract
-			if !contract.UseGas(evm.Accesses.TouchFullAccount(address.Bytes()[:], true)) {
-				err = ErrCodeStoreOutOfGas
-			}
-
 			if err == nil && len(ret) > 0 && !contract.UseGas(evm.Accesses.TouchCodeChunksRangeAndChargeGas(address.Bytes(), 0, uint64(len(ret)), uint64(len(ret)), true)) {
 				err = ErrCodeStoreOutOfGas
 			}
