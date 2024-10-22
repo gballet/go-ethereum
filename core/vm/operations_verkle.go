f3abb6896275fff6a357e1d0246ab9be680d304b
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-10-22 17:50:46 +0200

    fix: value transfer gas charge before 1/64th + warm costs for precomp + system contracts
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/vm/operations_verkle.go
+++ core/vm/operations_verkle.go
@@ -55,12 +55,24 @@ func gasExtCodeHash4762(evm *EVM, contract *Contract, stack *Stack, mem *Memory,
 
 func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
 	return func(evm *EVM, contract *Contract, stack *Stack, mem *Memory, memorySize uint64) (uint64, error) {
-		target := common.Address(stack.Back(1).Bytes20())
+		var (
+			target           = common.Address(stack.Back(1).Bytes20())
+			transfersValue   = !stack.Back(2).IsZero()
+			witnessGas       uint64
+			_, isPrecompile  = evm.precompile(target)
+			isSystemContract = evm.isSystemContract(target)
+		)
 
-		_, isPrecompile := evm.precompile(target)
-		isSystemContract := evm.isSystemContract(target)
-		if isPrecompile || isSystemContract {
-			return params.WarmStorageReadCostEIP2929, nil
+		// If value is transferred, it is charged before 1/64th
+		// is subtracted from the available gas pool.
+		if transfersValue {
+			wantedValueTransferWitnessGas := evm.Accesses.TouchAndChargeValueTransfer(contract.Address().Bytes()[:], target[:], contract.Gas)
+			if wantedValueTransferWitnessGas > contract.Gas {
+				return wantedValueTransferWitnessGas, nil
+			}
+			witnessGas = wantedValueTransferWitnessGas
+		} else if isPrecompile || isSystemContract {
+			witnessGas = params.WarmStorageReadCostEIP2929
 		}
 
 		// The charging for the value transfer is done BEFORE subtracting
@@ -68,16 +80,23 @@ func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
 		// (so before we get to this point)
 		// But the message call is part of the subcall, for which only 63/64th
 		// of the gas should be available.
-		wantedWitnessGas := evm.Accesses.TouchAndChargeMessageCall(target.Bytes(), contract.Gas)
-		if wantedWitnessGas > contract.Gas {
-			return wantedWitnessGas, nil
+		wantedMessageCallWitnessGas := evm.Accesses.TouchAndChargeMessageCall(target.Bytes(), contract.Gas-witnessGas)
+		var overflow bool
+		if witnessGas, overflow = math.SafeAdd(witnessGas, wantedMessageCallWitnessGas); overflow {
+			return 0, ErrGasUintOverflow
+		}
+		if witnessGas > contract.Gas {
+			return witnessGas, nil
 		}
 
 		gas, err := oldCalculator(evm, contract, stack, mem, memorySize)
 		if err != nil {
 			return 0, err
 		}
-		return wantedWitnessGas + gas, nil
+		if gas, overflow = math.SafeAdd(gas, witnessGas); overflow {
+			return 0, ErrGasUintOverflow
+		}
+		return gas, nil
 	}
 }
 
