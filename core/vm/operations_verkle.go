553047e177be048b5d3ac3f8b8e74c89f6460fd3
Ignacio Hagopian <jsign.uy@gmail.com>
2024-10-22 14:22:57 -0300

    fix
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- core/vm/operations_verkle.go
+++ core/vm/operations_verkle.go
@@ -101,11 +101,53 @@ func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
 	}
 }
 
+// TODO(verkle): clean up... maybe we can add another parameter in the other wrapper, but can get messy.
+func makeCallVariantGasEIP4762WithoutTransfer(oldCalculator gasFunc) gasFunc {
+	return func(evm *EVM, contract *Contract, stack *Stack, mem *Memory, memorySize uint64) (uint64, error) {
+		var (
+			target           = common.Address(stack.Back(1).Bytes20())
+			witnessGas       uint64
+			_, isPrecompile  = evm.precompile(target)
+			isSystemContract = evm.isSystemContract(target)
+		)
+
+		// If value is transferred, it is charged before 1/64th
+		// is subtracted from the available gas pool.
+		if isPrecompile || isSystemContract {
+			witnessGas = params.WarmStorageReadCostEIP2929
+		} else {
+			// The charging for the value transfer is done BEFORE subtracting
+			// the 1/64th gas, as this is considered part of the CALL instruction.
+			// (so before we get to this point)
+			// But the message call is part of the subcall, for which only 63/64th
+			// of the gas should be available.
+			wantedMessageCallWitnessGas := evm.Accesses.TouchAndChargeMessageCall(target.Bytes(), contract.Gas-witnessGas)
+			var overflow bool
+			if witnessGas, overflow = math.SafeAdd(witnessGas, wantedMessageCallWitnessGas); overflow {
+				return 0, ErrGasUintOverflow
+			}
+			if witnessGas > contract.Gas {
+				return witnessGas, nil
+			}
+		}
+
+		gas, err := oldCalculator(evm, contract, stack, mem, memorySize)
+		if err != nil {
+			return 0, err
+		}
+		var overflow bool
+		if gas, overflow = math.SafeAdd(gas, witnessGas); overflow {
+			return 0, ErrGasUintOverflow
+		}
+		return gas, nil
+	}
+}
+
 var (
 	gasCallEIP4762         = makeCallVariantGasEIP4762(gasCall)
-	gasCallCodeEIP4762     = makeCallVariantGasEIP4762(gasCallCode)
-	gasStaticCallEIP4762   = makeCallVariantGasEIP4762(gasStaticCall)
-	gasDelegateCallEIP4762 = makeCallVariantGasEIP4762(gasDelegateCall)
+	gasCallCodeEIP4762     = makeCallVariantGasEIP4762WithoutTransfer(gasCallCode)
+	gasStaticCallEIP4762   = makeCallVariantGasEIP4762WithoutTransfer(gasStaticCall)
+	gasDelegateCallEIP4762 = makeCallVariantGasEIP4762WithoutTransfer(gasDelegateCall)
 )
 
 func gasSelfdestructEIP4762(evm *EVM, contract *Contract, stack *Stack, mem *Memory, memorySize uint64) (uint64, error) {
