c329550addc41301f7fb1482390e3381c375f25f
Ignacio Hagopian <jsign.uy@gmail.com>
2024-10-22 16:45:14 -0300

    fix
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- core/state/access_witness.go
+++ core/state/access_witness.go
@@ -106,12 +106,12 @@ func (aw *AccessWitness) TouchAndChargeMessageCall(addr []byte, useGasFn UseGasF
 }
 
 func (aw *AccessWitness) TouchAndChargeValueTransfer(callerAddr, targetAddr []byte, useGasFn UseGasFn) bool {
-	_, ok := aw.touchAddressAndChargeGas(callerAddr, zeroTreeIndex, utils.BasicDataLeafKey, true, useGasFn)
+	chargedGas1, ok := aw.touchAddressAndChargeGas(callerAddr, zeroTreeIndex, utils.BasicDataLeafKey, true, useGasFn)
 	if !ok {
 		return false
 	}
-	chargedGas, ok := aw.touchAddressAndChargeGas(targetAddr, zeroTreeIndex, utils.BasicDataLeafKey, true, useGasFn)
-	return ok && (chargedGas > 0 || useGasFn(params.WarmStorageReadCostEIP2929))
+	chargedGas2, ok := aw.touchAddressAndChargeGas(targetAddr, zeroTreeIndex, utils.BasicDataLeafKey, true, useGasFn)
+	return ok && (chargedGas1+chargedGas2 > 0 || useGasFn(params.WarmStorageReadCostEIP2929))
 }
 
 // TouchAndChargeContractCreateCheck charges access costs before
