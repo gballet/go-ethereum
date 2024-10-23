a9b0e68964f1fe048b7909cd1c93d239c409d9c6
Ignacio Hagopian <jsign.uy@gmail.com>
2024-10-23 17:02:56 -0300

    Filling fixes (#516)
    * fix makeCallVariantGasEIP4762
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * fix
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * fix
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * fix
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * ci: target v0.0.6
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * improve fix
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    
    * add witness gas to "old" gas upon error
    
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>
    
    * workflow: remove target branch from list of branches
    
    ---------
    
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>
    Co-authored-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/vm/operations_verkle.go
+++ core/vm/operations_verkle.go
@@ -53,11 +53,10 @@ func gasExtCodeHash4762(evm *EVM, contract *Contract, stack *Stack, mem *Memory,
 	return evm.Accesses.TouchCodeHash(address[:], false, contract.Gas, true), nil
 }
 
-func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
+func makeCallVariantGasEIP4762(oldCalculator gasFunc, withTransferCosts bool) gasFunc {
 	return func(evm *EVM, contract *Contract, stack *Stack, mem *Memory, memorySize uint64) (uint64, error) {
 		var (
 			target           = common.Address(stack.Back(1).Bytes20())
-			transfersValue   = !stack.Back(2).IsZero()
 			witnessGas       uint64
 			_, isPrecompile  = evm.precompile(target)
 			isSystemContract = evm.isSystemContract(target)
@@ -65,7 +64,7 @@ func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
 
 		// If value is transferred, it is charged before 1/64th
 		// is subtracted from the available gas pool.
-		if transfersValue {
+		if withTransferCosts && !stack.Back(2).IsZero() {
 			wantedValueTransferWitnessGas := evm.Accesses.TouchAndChargeValueTransfer(contract.Address().Bytes()[:], target[:], contract.Gas)
 			if wantedValueTransferWitnessGas > contract.Gas {
 				return wantedValueTransferWitnessGas, nil
@@ -73,38 +72,39 @@ func makeCallVariantGasEIP4762(oldCalculator gasFunc) gasFunc {
 			witnessGas = wantedValueTransferWitnessGas
 		} else if isPrecompile || isSystemContract {
 			witnessGas = params.WarmStorageReadCostEIP2929
+		} else {
+			// The charging for the value transfer is done BEFORE subtracting
+			// the 1/64th gas, as this is considered part of the CALL instruction.
+			// (so before we get to this point)
+			// But the message call is part of the subcall, for which only 63/64th
+			// of the gas should be available.
+			wantedMessageCallWitnessGas := evm.Accesses.TouchAndChargeMessageCall(target.Bytes(), contract.Gas-witnessGas)
+			var overflow bool
+			if witnessGas, overflow = math.SafeAdd(witnessGas, wantedMessageCallWitnessGas); overflow {
+				return 0, ErrGasUintOverflow
+			}
+			if witnessGas > contract.Gas {
+				return witnessGas, nil
+			}
 		}
 
-		// The charging for the value transfer is done BEFORE subtracting
-		// the 1/64th gas, as this is considered part of the CALL instruction.
-		// (so before we get to this point)
-		// But the message call is part of the subcall, for which only 63/64th
-		// of the gas should be available.
-		wantedMessageCallWitnessGas := evm.Accesses.TouchAndChargeMessageCall(target.Bytes(), contract.Gas-witnessGas)
-		var overflow bool
-		if witnessGas, overflow = math.SafeAdd(witnessGas, wantedMessageCallWitnessGas); overflow {
-			return 0, ErrGasUintOverflow
-		}
-		if witnessGas > contract.Gas {
-			return witnessGas, nil
-		}
-
+		contract.Gas -= witnessGas
+		// if the operation fails, adds witness gas to the gas before returning the error
 		gas, err := oldCalculator(evm, contract, stack, mem, memorySize)
-		if err != nil {
-			return 0, err
-		}
+		contract.Gas += witnessGas // restore witness gas so that it can be charged at the callsite
+		var overflow bool
 		if gas, overflow = math.SafeAdd(gas, witnessGas); overflow {
 			return 0, ErrGasUintOverflow
 		}
-		return gas, nil
+		return gas, err
 	}
 }
 
 var (
-	gasCallEIP4762         = makeCallVariantGasEIP4762(gasCall)
-	gasCallCodeEIP4762     = makeCallVariantGasEIP4762(gasCallCode)
-	gasStaticCallEIP4762   = makeCallVariantGasEIP4762(gasStaticCall)
-	gasDelegateCallEIP4762 = makeCallVariantGasEIP4762(gasDelegateCall)
+	gasCallEIP4762         = makeCallVariantGasEIP4762(gasCall, true)
+	gasCallCodeEIP4762     = makeCallVariantGasEIP4762(gasCallCode, false)
+	gasStaticCallEIP4762   = makeCallVariantGasEIP4762(gasStaticCall, false)
+	gasDelegateCallEIP4762 = makeCallVariantGasEIP4762(gasDelegateCall, false)
 )
 
 func gasSelfdestructEIP4762(evm *EVM, contract *Contract, stack *Stack, mem *Memory, memorySize uint64) (uint64, error) {
